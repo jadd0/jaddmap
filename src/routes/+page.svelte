3ccc948e706a2fd67e2eec2ac1bbf8910f12c9bb
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-11-06 13:34:06 +0000

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Draggable from './components/draggable/+page.svelte';
+</script>
+
+<body>
+	<div id="canvas">
+		<Draggable>
+			<h1>Drag Me</h1>
+		</Draggable>
+	</div>
+</body>
---
+++ src/routes/components/draggable/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	export let left: number = 100;
+	export let top: number = 100;
+	
+	let moving: boolean = false;
+	
+	function onMouseDown() {
+		moving = true;
+	}
+	
+	function onMouseMove(e: any) {
+		if (moving) {
+			left += e.movementX;
+			top += e.movementY;
+		}
+	}
+	
+	function onMouseUp() {
+		moving = false;
+	}
+	
+// 	$: console.log(moving);
+</script>
+
+<style>
+	.draggable {
+		user-select: none;
+		cursor: move;
+		border: solid 1px gray;
+		position: absolute;
+	}
+</style>
+
+<section on:mousedown={onMouseDown} style="left: {left}px; top: {top}px;" class="draggable">
+	<slot></slot>
+</section>
+
+<svelte:window on:mouseup={onMouseUp} on:mousemove={onMouseMove} />
